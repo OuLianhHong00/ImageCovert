0d25a14cb8659bf0afd54ee849147c5f55ad8936
OuLianhong <841002907@qq.com>
2018-11-05 19:05:26 +0800

    基本处理

---
+++ ImageProcess/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ImageProcess
+{
+    partial class FrmImage
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.btnReverse = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.picAfter = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDiffusion = new System.Windows.Forms.Button();
+            this.btnSharpen = new System.Windows.Forms.Button();
+            this.btnMosaic = new System.Windows.Forms.Button();
+            this.btnHorse = new System.Windows.Forms.Button();
+            this.btnRelief = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAfter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.picOriginal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picOriginal.Location = new System.Drawing.Point(162, 28);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(352, 464);
+            this.picOriginal.TabIndex = 0;
+            this.picOriginal.TabStop = false;
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnOpenImage.Location = new System.Drawing.Point(24, 208);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(76, 46);
+            this.btnOpenImage.TabIndex = 1;
+            this.btnOpenImage.Text = "打开";
+            this.btnOpenImage.UseVisualStyleBackColor = false;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Location = new System.Drawing.Point(152, 539);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(91, 46);
+            this.btnReverse.TabIndex = 1;
+            this.btnReverse.Text = "底片";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(306, 539);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(91, 46);
+            this.btnGray.TabIndex = 1;
+            this.btnGray.Text = "灰度化";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // picAfter
+            // 
+            this.picAfter.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.picAfter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picAfter.Location = new System.Drawing.Point(817, 28);
+            this.picAfter.Name = "picAfter";
+            this.picAfter.Size = new System.Drawing.Size(352, 464);
+            this.picAfter.TabIndex = 0;
+            this.picAfter.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSave.Location = new System.Drawing.Point(24, 358);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(76, 46);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDiffusion
+            // 
+            this.btnDiffusion.Location = new System.Drawing.Point(460, 539);
+            this.btnDiffusion.Name = "btnDiffusion";
+            this.btnDiffusion.Size = new System.Drawing.Size(91, 46);
+            this.btnDiffusion.TabIndex = 1;
+            this.btnDiffusion.Text = "柔化";
+            this.btnDiffusion.UseVisualStyleBackColor = true;
+            this.btnDiffusion.Click += new System.EventHandler(this.btnDiffusion_Click);
+            // 
+            // btnSharpen
+            // 
+            this.btnSharpen.Location = new System.Drawing.Point(614, 539);
+            this.btnSharpen.Name = "btnSharpen";
+            this.btnSharpen.Size = new System.Drawing.Size(91, 46);
+            this.btnSharpen.TabIndex = 1;
+            this.btnSharpen.Text = "锐化";
+            this.btnSharpen.UseVisualStyleBackColor = true;
+            this.btnSharpen.Click += new System.EventHandler(this.btnSharpen_Click);
+            // 
+            // btnMosaic
+            // 
+            this.btnMosaic.Location = new System.Drawing.Point(768, 539);
+            this.btnMosaic.Name = "btnMosaic";
+            this.btnMosaic.Size = new System.Drawing.Size(91, 46);
+            this.btnMosaic.TabIndex = 1;
+            this.btnMosaic.Text = "马赛克";
+            this.btnMosaic.UseVisualStyleBackColor = true;
+            this.btnMosaic.Click += new System.EventHandler(this.btnMosaic_Click);
+            // 
+            // btnHorse
+            // 
+            this.btnHorse.Location = new System.Drawing.Point(922, 539);
+            this.btnHorse.Name = "btnHorse";
+            this.btnHorse.Size = new System.Drawing.Size(91, 46);
+            this.btnHorse.TabIndex = 1;
+            this.btnHorse.Text = "雾化";
+            this.btnHorse.UseVisualStyleBackColor = true;
+            this.btnHorse.Click += new System.EventHandler(this.btnHorse_Click);
+            // 
+            // btnRelief
+            // 
+            this.btnRelief.Location = new System.Drawing.Point(1076, 539);
+            this.btnRelief.Name = "btnRelief";
+            this.btnRelief.Size = new System.Drawing.Size(91, 46);
+            this.btnRelief.TabIndex = 1;
+            this.btnRelief.Text = "浮雕";
+            this.btnRelief.UseVisualStyleBackColor = true;
+            this.btnRelief.Click += new System.EventHandler(this.btnRelief_Click);
+            // 
+            // FrmImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1332, 651);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRelief);
+            this.Controls.Add(this.btnHorse);
+            this.Controls.Add(this.btnMosaic);
+            this.Controls.Add(this.btnSharpen);
+            this.Controls.Add(this.btnDiffusion);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.btnOpenImage);
+            this.Controls.Add(this.picAfter);
+            this.Controls.Add(this.picOriginal);
+            this.Name = "FrmImage";
+            this.Text = "图像处理";
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAfter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.Button btnReverse;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.PictureBox picAfter;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDiffusion;
+        private System.Windows.Forms.Button btnSharpen;
+        private System.Windows.Forms.Button btnMosaic;
+        private System.Windows.Forms.Button btnHorse;
+        private System.Windows.Forms.Button btnRelief;
+    }
+}
+
---
+++ ImageProcess/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/// <summary>
+/// 编写者：欧连红
+/// 实现功能：图片的打开、保存、底片、灰度、柔化、锐化、马赛克、雾化以及浮雕
+/// </summary>
+namespace ImageProcess
+{
+    //图片特效主界面
+    public partial class FrmImage : Form
+    {
+        public FrmImage()
+        {
+            InitializeComponent();
+        }
+        Image img;
+        //打开图片
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            //选择的文件名称
+            string imgFileName = ofd.FileName;
+            img = Image.FromFile(imgFileName);
+            picOriginal.BackgroundImage = img;
+
+        }
+        //底片效果
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(img);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color c = bmp.GetPixel(i, j);
+                    Color cAfter = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    bmp.SetPixel(i, j, cAfter);
+                }
+            }
+            picAfter.BackgroundImage = bmp;
+
+        }
+        //灰度化
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(img);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color c = bmp.GetPixel(i, j);
+                    int gray = (c.R + c.G + c.B) / 3;
+                    // int gray=(int)
+                    Color cAfter = Color.FromArgb(gray, gray, gray);
+                    bmp.SetPixel(i, j, cAfter);
+                }
+            }
+            picAfter.BackgroundImage = bmp;
+        }
+        //保存
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ShowDialog();
+
+            picAfter.BackgroundImage.Save(sfd.FileName);
+        }
+        //浮雕
+        private void btnRelief_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(img);
+            for (int i = 0; i < bmp.Width - 1; i++)
+            {
+                for (int j = 0; j < bmp.Height - 1; j++)
+                {
+                    Color c = bmp.GetPixel(i, j);
+                    Color a = bmp.GetPixel(i + 1, j + 1);
+                    int r = Math.Abs(c.R - a.R + 128);
+                    int g = Math.Abs(c.G - a.G + 128);
+                    int b = Math.Abs(c.B - a.B + 128);
+                    if (r > 255) r = 255;
+                    if (g > 255) g = 255;
+                    if (b > 255) b = 255;
+                    if (r < 0) r = 0;
+                    if (g < 0) g = 0;
+                    if (b < 0) b = 0;
+                    Color cAfter = Color.FromArgb(r, g, b);
+                    bmp.SetPixel(i, j, cAfter);
+                }
+            }
+            picAfter.BackgroundImage = bmp;
+        }
+       
+        //锐化
+        private void btnSharpen_Click(object sender, EventArgs e)
+        {
+            //拉普拉斯模板
+            int[] laplacian = { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
+            Bitmap bmp = new Bitmap(img);
+            for (int i = 1; i < bmp.Width - 1; i++)
+            {
+                for (int j = 1; j < bmp.Height - 1; j++)
+                {
+                    int r = 0, g = 0, b = 0;
+                    int index = 0;
+                    for (int col = -1; col <= 1; col++)
+                    {
+                        for (int row = -1; row <= 1; row++)
+                        {
+                            Color c = bmp.GetPixel(i + row, j + col);
+                            r += c.R * laplacian[index];
+                            g += c.G * laplacian[index];
+                            b += c.B * laplacian[index];
+                            index++;
+                        }
+                    }
+                    r = r > 255 ? 255 : r;
+                    r = r < 0 ? 0 : r;
+                    g = g > 255 ? 255 : g;
+                    g = g < 0 ? 0 : g;
+                    b = b > 255 ? 255 : b;
+                    b = b < 0 ? 0 : b;
+
+                    Color cAfter = Color.FromArgb(r, g, b);
+                    bmp.SetPixel(i - 1, j - 1, cAfter);
+                }
+            }
+            picAfter.BackgroundImage = bmp;
+        }
+        //雾化
+        private void btnHorse_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(img);
+            for (int i = 1; i < bmp.Width - 1; i++)
+            {
+                for (int j = 1; j < bmp.Height - 1; j++)
+                {
+                    Random myRandom = new Random();
+                    int k = myRandom.Next(123456);
+                    int dx = i + k % 19;
+                    if (dx >= bmp.Width)
+                        dx = bmp.Width - 1;
+                    int dy = j + k % 19;
+                    if (dy >= bmp.Height)
+                        dy = bmp.Height - 1;
+                    Color c = bmp.GetPixel(dx, dy);
+                    Color cAfter = Color.FromArgb(c.R, c.G, c.B);
+                    bmp.SetPixel(i, j, cAfter);
+                }
+            }
+            picAfter.BackgroundImage = bmp;
+        }
+        //马赛克
+        private void btnMosaic_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(img);
+            Single maWidth = 1f, maHeight = 1f;
+            int effectWidth = 20;
+            for (int i = 0; i < bmp.Height * maHeight; i += effectWidth)
+            {
+                for (int j = 0; j < bmp.Width * maWidth; j += effectWidth)
+                {
+                    int r = 0, g = 0, b = 0;
+                    int bluePixelCount = 0;
+                    for (int x = j; (x < j + effectWidth && x < bmp.Width); x++)
+                    {
+                        for (int y = i; (y < i + effectWidth && y < bmp.Height); y++)
+                        {
+                            Color c = bmp.GetPixel(x, y);
+                            r += c.R;
+                            g += c.G;
+                            b += c.B;
+                            bluePixelCount++;
+                        }
+                    }
+                    r = r / bluePixelCount;
+                    g = g / bluePixelCount;
+                    b = b / bluePixelCount;
+                    for (int x = j; (x < j + effectWidth && x < bmp.Width); x++)
+                    {
+                        for (int y = i; (y < i + effectWidth && y < bmp.Height); y++)
+                        {
+                            Color cAfter = Color.FromArgb(r, g, b);
+                            bmp.SetPixel(x, y, cAfter);
+                        }
+                    }
+                }
+
+            }
+            picAfter.BackgroundImage = bmp;
+        }
+        //柔化
+        private void btnDiffusion_Click(object sender, EventArgs e)
+        {
+            int[] Gauss = { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
+            Bitmap bmp = new Bitmap(img);
+            for (int i = 1; i < bmp.Width-1; i++) {
+                for (int j = 1; j < bmp.Height-1; j++) {
+                    int r = 0, g = 0, b = 0;
+                    int index = 0;
+                    for (int col = -1; col <= 1; col++) {
+                        for (int row = -1; row <= 1; row++) {
+                            Color c = bmp.GetPixel(i+row,j+col);
+                            r += c.R * Gauss[index];
+                            g += c.R * Gauss[index];
+                            b += c.R + Gauss[index];
+                            index++;
+                        }
+                    }
+                    r /= 16;
+                    g /=16;
+                    b /= 16;
+                    r = r > 255 ? 255 : r;
+                    r = r < 0 ? 0 : r;
+                    g = g > 255 ? 255 : g;
+                    g = g < 0 ? 0 : g;
+                    b = b > 255 ? 255 : b;
+                    b = b < 0 ? 0 : b;
+                    Color cAfter = Color.FromArgb(r, g, b);
+                    bmp.SetPixel(i-1, j-1, cAfter);
+                }
+            }
+            picAfter.BackgroundImage = bmp;
+        }
+       
+    }
+}
+
